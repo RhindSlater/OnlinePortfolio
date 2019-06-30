9cdf51ec6a2f1dd8afb4ff0de48f23308c13a0b1
RhindSlater <rhindslater@gmail.com>
2019-06-30 12:30:41 +1200

    Online Portfolio Initial

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+$(".homeSouth").click(function() {
+    var offset = 60;
+    if($(window).width() < 992) {
+        offset = 54;
+    }
+    $('html,body').animate({
+        scrollTop: $(".aboutMe").offset().top - offset},'slow');
+});
+
+var mouseOverToggle = 1;
+function mouseOver() {
+    mouseOverToggle++;
+
+    if(mouseOverToggle % 2 == 0) {
+        $('.backBlur').css({
+            "-webkit-transition":"all 0.3s ease-out",
+            "transition":"all 0.3s ease-out",
+            "background-color":"rgba(0,0,0,0)"
+        });
+    }
+    else {
+        $('.backBlur').css({
+            "-webkit-transition":"all 0.3s ease-out",
+            "transition":"all 0.3s ease-out",
+            "background-color":"rgba(0,0,0,0.5)"
+        });
+    }
+}
